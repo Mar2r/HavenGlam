9bf1e67a8cc71c34d14ccfd1da703207c74058aa
Mar2r <ramosmargarita2m@gmail.com>
2026-07-30 21:47:53 -0600

    Se creo el proyecto con configuracion

---
+++ src/main/java/org/esfe/HavenGlam/HavenGlamApplication.java
@@ -0,0 +1,13 @@
+package org.esfe.HavenGlam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HavenGlamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HavenGlamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/esfe/HavenGlam/HavenGlamApplicationTests.java
@@ -0,0 +1,13 @@
+package org.esfe.HavenGlam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HavenGlamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
